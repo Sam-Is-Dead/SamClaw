8088f6c316525d261195a80dc437f99a4ee2e7e6
Samarth Fatake <samarth.g.fatake@gmail.com>
2026-07-07 07:14:06 +0530

    Setting up CLI Mode

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-console.log("Hello via Bun!");
+#!/usr/bin/env bun
+
+import {Command} from "commander";
+import {runWakeUp} from "./tui/wakeup.ts";
+
+const program = new Command();
+
+program
+  .name("SamClaw")
+  .description("A simple CLI tool")
+  .version("1.0.0");
+
+program
+    .command("wakeup")
+    .description("Wake up the system")
+    .action(
+        async() => {
+        console.log("Waking up the system...");
+        await runWakeUp();
+      });
+
+    await program.parseAsync(process.argv);
